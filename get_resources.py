08ea81cb313da9a24270138b730260998de16c84
smurashov <smurashov@mirantis.com>
2014-11-28 11:01:55 +0300

    Chanche test config generation

--- get_resources.py
+++ get_resources.py
@@ -1,6 +1,4 @@
 import argparse
-import requests
-import json
 
 
 parser = argparse.ArgumentParser(description="Get resources for test config")
@@ -11,30 +9,14 @@ parser.add_argument("-pumphouse_url", dest='pumphouse_url', type=str,
 
 args = parser.parse_args()
 
-req = requests.get("%s/resources" % args.pumphouse_url).text
-req = json.loads(req)
-tenant = next(
-    i for i in req['source']['resources']
-    if i['type'] == 'tenant' and i['data']['name'] == 'admin')['id']
-
-host = next(
-    i for i in req['source']['resources'] if i['type'] == 'host')['id']
-
-myconfig = {
-    "endpoint": args.pumphouse_url,
-    "timeout": 120,
-    "cases": ["reset", "migrate", "evaculate"],
-    "tenant": {
-        "id": tenant,
-        "type": "tenant",
-        "cloud": "source"
-    },
-    "host": {
-        "id": host,
-        "type": "host",
-        "cloud": "source"
-    }
-}
 with open('config.js', 'w') as config:
+    config.write("/*jslint node:true*/\n\n")
+    config.write("'use strict';\n\n")
+
     config.write(
-        "module.exports = " + json.dumps(myconfig, config, indent=4) + ";")
+        "module.exports = {\n")
+    config.write("    endpoint: '%s',\n" % args.pumphouse_url)
+    config.write("    timeout: 2500,\n")
+    config.write("    cases: [\n")
+    config.write("        'reset',\n        'migrate'\n    ],\n")
+    config.write("    tenant_name_mask: 'pumphouse-'\n};")
