b28b5c0c08c4b1b66191b98339a79226f4c4e893
smurashov <smurashov@mirantis.com>
2014-12-02 12:50:44 +0300

    Add all needed nailgun methods

--- fuel/nailgun.py
+++ fuel/nailgun.py
@@ -15,31 +15,30 @@ class NailgunClient(object):
 
     def _get_cluster_list(self):
         endpoint = urlparse.urljoin(self.url, "api/clusters")
-        return requests.get(endpoint, headers=self.headers)
+        return requests.get(endpoint, headers=self.headers).json()
 
     def _get_cluster(self, cluster_id):
         endpoint = urlparse.urljoin(self.url,
                                     "api/clusters/{0}".format(cluster_id))
-        return requests.get(endpoint, headers=self.headers)
+        return requests.get(endpoint, headers=self.headers).json()
 
     def _get_cluster_attributes(self, cluster_id):
         endpoint = urlparse.urljoin(self.url,
                                     "api/clusters/{0}/attributes".format(
                                         cluster_id))
-        return requests.get(endpoint, headers=self.headers)
+        return requests.get(endpoint, headers=self.headers).json()
 
     def _get_list_nodes(self):
         endpoint = urlparse.urljoin(self.url, "api/nodes")
-        return requests.get(endpoint, headers=self.headers)
+        return requests.get(endpoint, headers=self.headers).json()
 
     def _get_list_networks(self, cluster_id):
-        net_provider = json.loads(self._get_cluster(cluster_id).content)[
-            "net_provider"]
+        net_provider = self._get_cluster(cluster_id)["net_provider"]
         endpoint = urlparse.urljoin(self.url,
                                     "/api/clusters/{0}"
                                     "/network_configuration/{1}".format(
                                         cluster_id, net_provider))
-        return requests.get(endpoint, headers=self.headers)
+        return requests.get(endpoint, headers=self.headers).json()
 
     def _create_cluster(self, data):
         endpoint = urlparse.urljoin(self.url, "api/clusters")
@@ -53,8 +52,40 @@ class NailgunClient(object):
     def update_cluster_attributes(self, cluster_id, attrs):
         endpoint = urlparse.urljoin(
             self.url, "api/clusters/{}/attributes".format(cluster_id))
-        return requests.put(endpoint, headers=self.headers, data=attrs).json()
+        return requests.put(endpoint, headers=self.headers, data=attrs)
 
     def update_node(self, node_id, data):
         endpoint = urlparse.urljoin(self.url, "api/nodes/{}".format(node_id))
-        return requests.put(endpoint, headers=self.headers, data=data).json()
+        return requests.put(endpoint, headers=self.headers, data=data)
+
+    def get_node_interfaces(self, node_id):
+        endpoint = urlparse.urljoin(self.url,
+                                    "api/nodes/{}/interfaces".format(node_id))
+        return requests.get(endpoint, headers=self.headers).json()
+
+    def put_node_interfaces(self, data):
+        """
+
+        :param data: [{'id': node_id, 'interfaces': interfaces}]
+        :return: response
+        """
+        endpoint = urlparse.urljoin(self.url, "api/nodes/interfaces")
+        return requests.put(endpoint, headers=self.headers, data=data)
+
+    def update_cluster_networks(self, cluster_id, data):
+        net_provider = self._get_cluster(cluster_id)["net_provider"]
+        endpoint = urlparse.urljoin(
+            self.url,
+            "api/clusters/{}/network_configuration/{}".format(cluster_id,
+                                                              net_provider))
+        return requests.put(endpoint, headers=self.headers, data=data)
+
+    def get_node_disks(self, node_id):
+        endpoint = urlparse.urljoin(self.url,
+                                    "api/nodes/{}/disks".format(node_id))
+        return requests.get(endpoint, headers=self.headers).json()
+
+    def put_node_disks(self, node_id, data):
+        endpoint = urlparse.urljoin(self.url,
+                                    "api/nodes/{}/disks".format(node_id))
+        return requests.put(endpoint, headers=self.headers, data=data)
